12b410b051157f020732f8c37f47c5fe378179ca
AmarHasecic <ahasecic1@etf.unsa.ba>
2023-03-09 19:05:03 +0100

    connection to the mqtt broker and controling servo when message is recieved

---
+++ arduino /fish-feeder.cpp
@@ -0,0 +1,101 @@
+
+#include <ESP8266WiFi.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+#include <WiFiClientSecure.h>
+#include<Servo.h>
+
+Servo motor;
+
+const char* ssid = "";
+const char* password = "";
+
+
+// MQTT broker credentials
+#define MQTT_SERVER "io.adafruit.com"
+#define MQTT_PORT 1883
+#define MQTT_USERNAME ""
+#define MQTT_PASSWORD ""
+
+// MQTT client object and topics
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_USERNAME, MQTT_PASSWORD);
+//Adafruit_MQTT_Publish mqttPublisher = Adafruit_MQTT_Publish(&mqtt, "amar_hasecic/feeds/buttonNahrani");
+Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, "amar_hasecic/feeds/buttonNahrani");
+
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  delay(10);
+  Serial.println("Starting setup procedure...");
+
+    WiFi.begin(ssid, password);
+     while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+  }
+
+  if(WiFi.status() == WL_CONNECTED)
+  Serial.println("Connected to the network!");
+
+  // Connect to Adafruit IO MQTT server
+  mqttConnect();
+  
+  if(mqtt.subscribe(&onoffbutton) == true)
+  Serial.println("Ready to receive messages");
+
+  motor.attach(9);
+
+}
+
+int connectionsCounter=0;
+  
+void loop() {
+
+  if(connectionsCounter == 2147483647) connectionsCounter = 0;
+
+  if(connectionsCounter==0)
+     mqtt.disconnect();
+     
+  mqttConnect();
+  connectionsCounter++;
+
+ 
+    Adafruit_MQTT_Subscribe *subscription;
+    while((subscription = mqtt.readSubscription(1000))) {
+         if(subscription == &onoffbutton){
+
+          Serial.println("Feeding time :)");
+          //motor.write(90);
+         }
+      
+    }
+    
+
+  delay(4000);
+  if(! mqtt.ping()){
+    mqtt.disconnect();
+  }
+}
+
+
+
+void mqttConnect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+  }
+  Serial.println("MQTT Connected!");
+}
